c0d87884dfd74e65d1a5a5cc666ca74e628c5d34
hanul1106 <hanul1106@nearsolution.co.kr>
2024-04-13 20:43:03 +0900

    0413 commit 2

---
+++ src/lang/string/CharArrayMain.java
@@ -0,0 +1,10 @@
+package lang.string;
+
+public class CharArrayMain {
+    public static void main(String[] args) {
+        char a = 'a';
+        System.out.println("a = " + a);
+        char[] charArrays = new char[]{'h','e','l','l','o'};
+        System.out.println(charArrays);
+    }
+}
---
+++ src/lang/string/StringBasicMain.java
@@ -0,0 +1,11 @@
+package lang.string;
+
+public class StringBasicMain {
+    public static void main(String[] args) {
+        //int, boolean,char -> 소문자로 시작 하여 primitive type
+        String str = "hello";
+        String str2 = new String("hello");
+        System.out.println("str2 = " + str2);
+        System.out.println("str = " + str);
+    }
+}
