19c11e508b91d998cbc777dbad84f927665edfb2
hanul1106 <hanul1106@nearsolution.co.kr>
2024-11-14 19:35:31 +0900

    1114 commit

---
+++ src/collection/set/HashStart5.java
@@ -0,0 +1,55 @@
+package collection.set;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class HashStart5 {
+    static final int CAPACITY=11;
+    public static void main(String[] args) {
+    //  {1,2,5,8,14,99,9}
+      LinkedList<Integer>[] buckets = new LinkedList[CAPACITY];
+        System.out.println("buckets="+Arrays.toString(buckets));
+        for (int i = 0; i < CAPACITY; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+        System.out.println("buckets="+Arrays.toString(buckets));
+
+        add(buckets,1);
+        add(buckets,2);
+        add(buckets,5);
+        add(buckets,8);
+        add(buckets,14);
+        add(buckets,99);
+        add(buckets,11);
+        add(buckets,9); //중복
+
+
+        System.out.println(Arrays.toString(buckets));
+
+        //검색
+        int searchValue = 9;
+        System.out.println("buckets.contains("+searchValue+") = "+contains(buckets,searchValue));
+        System.out.println("buckets.contains(222) = "+contains(buckets,222));
+
+    }
+
+    private static boolean contains(LinkedList<Integer>[] buckets, int value){
+        return buckets[hashIndex(value)].contains(value);
+    }
+
+
+    private static void add(LinkedList<Integer>[] buckets, int value){
+        int idx = hashIndex(value);
+        LinkedList<Integer> bucket = buckets[idx];  //O(1)
+        if(!bucket.contains(value)){    //O(n)
+            bucket.add(value);
+        }
+
+    }
+    private static int hashIndex(int value){
+        return value%CAPACITY;
+    }
+
+}
