5f40fae487481ad7fc135cf974f729563b6c5eb5
hanul1106 <hanul1106@nearsolution.co.kr>
2025-02-05 19:48:30 +0900

    0205 commit

--- src/thread/control/yield/YieldMain.java
+++ src/thread/control/yield/YieldMain.java
@@ -19,7 +19,7 @@ public class YieldMain {
             for (int i = 0; i < 10; i++) {
                 System.out.println(Thread.currentThread().getName()+"-"+i);
                 //1.empty
-                sleep(1); //2. sleep : runnable -> timed waiting 으로 변경 1ms 지난 후 실행 스케줄링으로 넣음
+//                sleep(1); //2. sleep : runnable -> timed waiting 으로 변경 1ms 지난 후 실행 스케줄링으로 넣음
                 Thread.yield(); //3.yield : 현재 실행중인 스레드가 cpu를 양보하도록
             }
         }
---
+++ src/thread/sync/BankAccount.java
@@ -0,0 +1,7 @@
+package thread.sync;
+
+public interface BankAccount {
+    boolean withdraw(int amount);   //출금
+
+    int getBalance();   //잔고확인
+}
